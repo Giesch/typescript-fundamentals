a5a5d5cde0eec6abddb2fe9774100e5b48c94b99
Dan Knutson <dknutson@sitepen.com>
2019-02-26 09:25:07 -0600

    add stack and fib exercises

--- exercises/fib/src/fib.ts
+++ exercises/fib/src/fib.ts
@@ -3,6 +3,13 @@
  * Implement a generator function that can be used
  * to generate numbers in the Fibonacci Sequence
  */
-// export function* getFibSequence(): IterableIterator<number> {
- 
-// }
+export function* getFibSequence(): IterableIterator<number> {
+  let current = 1;
+  let previous = 0;
+  while (true) {
+    yield current;
+    let next = current + previous;
+    [previous, current] = [current, next];
+  }
+}
+
--- exercises/stack/src/stack.ts
+++ exercises/stack/src/stack.ts
@@ -6,6 +6,48 @@ interface IStack<T> {
   print(): void;
 }
 
-// export class Stack<T> {
+export class Stack<T> implements IStack<T> {
 
-// }
+  private currentNode?: StackNode<T>;
+
+  push(item: T): Stack<T>;
+  push(items: T[]): Stack<T>; // whyyy
+  push(stuff: T | T[]): Stack<T> {
+    // This is broken if T is an Array; stuff is unpacked when it shouldn't be
+    if (stuff instanceof Array) {
+      stuff.forEach(item => this.push(item));
+    } else {
+      const newstack = { item: stuff, next: this.currentNode };
+      this.currentNode = newstack;
+    }
+
+    return this;
+  }
+
+  pop() {
+    const node = this.currentNode;
+    if (!node) {
+      return undefined;
+    }
+    this.currentNode = node.next;
+    return node.item;
+  }
+
+  length() {
+    let l = 0;
+    let node = this.currentNode;
+    while (node) {
+      l++;
+      node = node.next;
+    }
+
+    return l;
+  }
+
+  print() {}
+}
+
+interface StackNode<T> {
+  item: T;
+  next?: StackNode<T>;
+}
